624734f2752b8715e2fd61cfec11193532dc4318
Chris Coveney <xkickflip@gmail.com>
2017-01-10 18:56:42 -0800

    Disable external entities & DTDs on the XML Parser for security
    Having external resources enabled means that it is possible to use
    a DTD crafted to extract details from local system files and serve
    them out as params to the external DTD location.  It's also possible
    to DOS a system by loading resources like /dev/random via the XML.
    
    This can be accomplished by unpacking an ACS file, ammending it's
    TOC file to include the above exploits, and then repacking it. If
    this file is opened by a system using this ACS library it would
    then make the external DTD requests potentially leaking system
    info.
    
    details here:
    https://www.owasp.org/index.php/XML_External_Entity_(XXE)_Processing
    
    Proctecting against this by disabling external entities and DTDs
    via various settings on DocumentBuilder before using it to parse
    the TOC file.
    
    Using methods from here:
    https://www.owasp.org/index.php/XML_External_Entity_(XXE)_Prevention_Cheat_Sheet

--- src/main/org/cytobank/acs/core/TableOfContents.java
+++ src/main/org/cytobank/acs/core/TableOfContents.java
@@ -170,17 +170,88 @@ public class TableOfContents extends AdditionalInfoElementWrapper {
 	 */
 	protected void parseXml(InputStream tableOfContentsXmlStream) throws InvalidIndexException, IOException, URISyntaxException, InvalidAssociationException, DuplicateFileResourceIdentifierException, SAXException {
 		try {
-			DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = dbfac.newDocumentBuilder();
+
+			DocumentBuilder docBuilder = getDocumentBuilder();
+
 			tableOfContentsDoc = docBuilder.parse(tableOfContentsXmlStream);
 			element = tableOfContentsDoc.getDocumentElement();
 			setupFileResourceIdentifiers();
 			setupAdditionalInfo();
+
 		} catch (ParserConfigurationException pce) {
 			throw new InvalidIndexException(pce.toString());
 		}
 
 	}
+
+
+	/**
+	 * <p>Creates a DocumentBuilder with Cytobank's preferred security settings
+	 * applied to it. Specifically turning off external entities and external
+	 * DTDs to prevent External Entity Exploits (XXE)</p>
+	 *
+	 * @throws ParserConfigurationException
+	 * @return DocumentBuilder
+	 */
+	protected DocumentBuilder getDocumentBuilder() throws ParserConfigurationException {
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+		DocumentBuilder db = null;
+
+		String FEATURE = null;
+
+		// This is the PRIMARY defense. If DTDs (doctypes) are disallowed, almost all XML entity attacks are prevented
+		// Xerces 2 only - http://xerces.apache.org/xerces2-j/features.html#disallow-doctype-decl
+		FEATURE = "http://apache.org/xml/features/disallow-doctype-decl";
+		dbf.setFeature(FEATURE, true);
+
+		// If you can't completely disable DTDs, then at least do the following:
+		// Xerces 1 - http://xerces.apache.org/xerces-j/features.html#external-general-entities
+		// Xerces 2 - http://xerces.apache.org/xerces2-j/features.html#external-general-entities
+		// JDK7+ - http://xml.org/sax/features/external-general-entities
+		FEATURE = "http://xml.org/sax/features/external-general-entities";
+		dbf.setFeature(FEATURE, false);
+
+		// Xerces 1 - http://xerces.apache.org/xerces-j/features.html#external-parameter-entities
+		// Xerces 2 - http://xerces.apache.org/xerces2-j/features.html#external-parameter-entities
+		// JDK7+ - http://xml.org/sax/features/external-parameter-entities
+		FEATURE = "http://xml.org/sax/features/external-parameter-entities";
+		dbf.setFeature(FEATURE, false);
+
+		// Disable external DTDs as well
+		FEATURE = "http://apache.org/xml/features/nonvalidating/load-external-dtd";
+		dbf.setFeature(FEATURE, false);
+
+		// and these as well, per Timothy Morgan's 2014 paper: "XML Schema, DTD, and Entity Attacks" (see reference below)
+		dbf.setXIncludeAware(false);
+		dbf.setExpandEntityReferences(false);
+
+		// And, per Timothy Morgan: "If for some reason support for inline DOCTYPEs are a requirement, then
+		// ensure the entity settings are disabled (as shown above) and beware that SSRF attacks
+		// (http://cwe.mitre.org/data/definitions/918.html) and denial
+		// of service attacks (such as billion laughs or decompression bombs via "jar:") are a risk."
+
+		boolean namespaceAware = true;
+		boolean xsdValidate = false;
+		boolean ignoreWhitespace = false;
+		boolean ignoreComments = false;
+		boolean putCDATAIntoText = false;
+		boolean createEntityRefs = false;
+
+		dbf.setNamespaceAware(namespaceAware);
+		dbf.setValidating(xsdValidate);
+		dbf.setIgnoringComments(ignoreComments);
+		dbf.setIgnoringElementContentWhitespace(ignoreWhitespace);
+		dbf.setCoalescing(putCDATAIntoText);
+		dbf.setExpandEntityReferences(createEntityRefs);
+
+		db = dbf.newDocumentBuilder();
+
+		return db;
+
+	}
+
 
 	/**
 	 * Returns the <code>ACS</code> instance that this <code>TableOfContents</code> is owned by.
